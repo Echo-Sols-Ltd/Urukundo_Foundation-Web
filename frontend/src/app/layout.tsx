b424063670133f1243591e7405060478b2532044
Byukusenge-Andrew <andrewbyukusenge4@gmail.com>
2025-09-15 16:16:20 +0200

    Add layout component for RootLayout and metadata configuration in Next.js

---
+++ frontend/src/app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from 'next';
+import './globals.css';
+import { AuthProvider } from '../contexts/AuthContext';
+
+export const metadata: Metadata = {
+  title: 'Urukundo Foundation - Share Love, Save Lives',
+  description:
+    'At Urukundo Foundation, we believe that technology and compassion together can bridge the gap between those who want to give and those in need.',
+  keywords: ['charity', 'donation', 'helping others'],
+  authors: [
+    {
+      name: 'Echo Sols',
+    },
+  ],
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className="antialiased">
+      <body>
+        <AuthProvider>{children}</AuthProvider>
+      </body>
+    </html>
+  );
+}
